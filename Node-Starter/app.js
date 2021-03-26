d77675e9fe40bf45916b772a4b89a9793597b513
Dineshkumar Balan <dineshkumarbalan.m@gmail.com>
2021-03-26 09:33:40 +0530

    adding simple local module

--- Node-Starter/app.js
+++ Node-Starter/app.js
@@ -7,7 +7,7 @@ http.createServer(function(req,res) {
 
     console.log("Http Request received on server from " + req.rawHeaders);
     res.writeHead(200,{'Content-Type':'application/json'});
-    var response = JSON.stringify({'app':'Node - Starter' + mod.find(10)});
+    var response = JSON.stringify({'app':'Node - Starter = ' + mod.find(10)});
 
     res.end(response);
 }).listen(8075);
